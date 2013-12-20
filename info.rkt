16e9e83504828e9e959f993e1f7a9ac79b4e77bc
Spencer Florence <florence@ccs.neu.edu>
2013-12-20 12:50:26 -0500

    added missing package dependency

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "draw-lib" "scribble-lib"))
+(define deps '("base" "draw-lib" "scribble-lib" "srfi-lite-lib"))
 (define build-deps '("draw-doc" "gui-doc" "gui-lib" "racket-doc"))
