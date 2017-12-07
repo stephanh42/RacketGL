3038c918abe937c40b67d0cd14a618a102e0383d
david-vanderson <david.vanderson@gmail.com>
2017-12-07 09:48:21 -0500

    don't run load-get-proc-address on every proc lookup

--- opengl/main.rkt
+++ opengl/main.rkt
@@ -29,10 +29,12 @@
           (delay 
             (case stype
               [(windows)
+               (define lib (ffi-lib "opengl32"))
+               (define GetProcAddr (load-get-proc-address lib '("GetProcAddress")))
+               (define wglGetProcAddr (load-get-proc-address lib '("wglGetProcAddress")))
                (λ (name)
-                 (define lib (ffi-lib "opengl32"))
-                 (or ((load-get-proc-address lib '("GetProcAddress")) name)
-                     ((load-get-proc-address lib '("wglGetProcAddress")) name)))]
+                 (or (GetProcAddr name)
+                     (wglGetProcAddr name)))]
               [(macosx)
                (load-get-proc-address (ffi-lib "/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/libGL")
                                       '())]
